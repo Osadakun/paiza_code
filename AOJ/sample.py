160516f154fb0eb487d45f3e570f565c4c303d07
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:36:14 +0900

    Leet文字列

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,25 +1,9 @@
-M = int(input())
-l_M = []
-flag = True
-for i in range(M):
-    x = int(input())
-    l_M.append(x)
-N = int(input())
-l_N = []
-for i in range(N):
-    x = int(input())
-    l_N.append(x)
-for i in range(1,32):
-    if i in l_M and i not in l_N:
-        print("A")
-    elif i not in l_M and i in l_N:
-        print("B")
-    elif i in l_M and i in l_N:
-        if(flag):
-            print("A")
-            flag = False
-        else:
-            print("B")
-            flag = True
-    else:
-        print("x")
+x = input()
+x = x.replace('O','0')
+x = x.replace('I','1')
+x = x.replace('Z','2')
+x = x.replace('E','3')
+x = x.replace('A','4')
+x = x.replace('Z','5')
+x = x.replace('G','6')
+print(x)
