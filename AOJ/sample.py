bf916a63125ca4ff6494bde58a5943eaaefed112
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:34:52 +0900

    残り物の量

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,11 +1,4 @@
-N = int(input())
-a,b = map(int, input().split())
-
-l = [False]*(N +max(a,b))
-l[0] = True
-for i in range(N):
-    if l[i]:
-        l[i+a] = True
-        l[i+b] = True
-ans = l[:N].count(False)
-print(ans)
+m,q,p = map(float, input().split())
+m -= (q*0.01) * m
+m -= (p*0.01) * m
+print(m)
