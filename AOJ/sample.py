4bf8a3610cd9504952eab2c4b3d0879a79fec585
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:32:50 +0900

    ロボット芸人

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,4 +1,16 @@
-i = input()
-vowel = 'aeiouAEIOU'
-cname = ''.join(s for s in i if s not in vowel)
-print(cname)
+N,M = map(int,input().split())
+l = []
+for i in range(M):
+    a,b = input().split()
+    a = int(a)
+    l.append([a,b])
+for i in range(1,N+1):
+    st = ""
+    for j in range(M):
+        if i % l[j][0] == 0:
+            st = st + l[j][1] + " "
+    if st == "":
+        print(i)
+    else:
+        st = st.rstrip(" ")
+        print(st)
