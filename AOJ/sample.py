56a794cdca44e1bbadaedeaa2c872b601def38b0
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:39:32 +0900

    プレゼント応募企画の実施

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,20 +1,10 @@
-# Let's チャレンジ！！
-n,m = map(int,input().split())
-l = []
-ans = []
-for i in range(n):
-    a = list((map(int, input().split())))
-    l.append(a)
-
-for i in range(n):
-    for j in range(n):
-        x = l[j][i] - m
-        if x < 0:
-            if j == 2:
-                ans.append(i+1)
-        else:
-            break
-if len(ans) == 0:
-    print("wait")
-else:
-    print(*ans)
+a = list(map(int,input().split()))
+for i in range(1,a[0]+1):
+    if i % a[1] == 0 and i % a[2] != 0:
+        print("A")
+    elif i % a[1] != 0 and i % a[2] == 0:
+        print("B")
+    elif i % a[1] == 0 and i % a[2] == 0:
+        print("AB")
+    else:
+        print("N")
