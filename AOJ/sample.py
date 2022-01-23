6936dbd3ce3ce7c1adb97057db5c217f2e789676
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:34:26 +0900

    階段登り

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,5 +1,11 @@
-i = input()
-x = "+" * (len(i) +2)
-print(x)
-print("+%s+" %i)
-print(x)
+N = int(input())
+a,b = map(int, input().split())
+
+l = [False]*(N +max(a,b))
+l[0] = True
+for i in range(N):
+    if l[i]:
+        l[i+a] = True
+        l[i+b] = True
+ans = l[:N].count(False)
+print(ans)
