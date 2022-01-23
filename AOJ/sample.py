05854e42d0fe4943dc4ef525558eca8a6cdd309a
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:35:29 +0900

    行きたいライブのスケジュール

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,4 +1,25 @@
-m,q,p = map(float, input().split())
-m -= (q*0.01) * m
-m -= (p*0.01) * m
-print(m)
+M = int(input())
+l_M = []
+flag = True
+for i in range(M):
+    x = int(input())
+    l_M.append(x)
+N = int(input())
+l_N = []
+for i in range(N):
+    x = int(input())
+    l_N.append(x)
+for i in range(1,32):
+    if i in l_M and i not in l_N:
+        print("A")
+    elif i not in l_M and i in l_N:
+        print("B")
+    elif i in l_M and i in l_N:
+        if(flag):
+            print("A")
+            flag = False
+        else:
+            print("B")
+            flag = True
+    else:
+        print("x")
