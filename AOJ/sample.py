92cd3befc6073f6c4164f19e8c04dbdfa282b92c
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:36:54 +0900

    バズの検出

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,9 +1,18 @@
-x = input()
-x = x.replace('O','0')
-x = x.replace('I','1')
-x = x.replace('Z','2')
-x = x.replace('E','3')
-x = x.replace('A','4')
-x = x.replace('Z','5')
-x = x.replace('G','6')
-print(x)
+n,m,t,k = map(int,input().split()) # 発言数，監視時間，対象期間，回数
+cnt = 1+m-t
+l = [[0 for j in range(m)] for i in range(n)]
+for i in range(m):
+    kom = list(map(int,input().split()))
+    for j in range(n):
+        l[j][i] = kom[j]
+for i in range(n):
+    for j in range(cnt):
+        l_a = l[i]
+        ans = sum(l_a[0:t])
+        l[i].remove(l[i][0])
+        if k - ans<= 0:
+            print("yes %d" %(j+t))
+            break
+        else:
+            if j == cnt-1:
+                print("no 0")
