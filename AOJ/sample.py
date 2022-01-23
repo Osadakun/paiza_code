70b210641f9d46ba1c7feffb9e79b7ec3a064687
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:33:25 +0900

    折り紙の貼り合わせ

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,16 +1,8 @@
-N,M = map(int,input().split())
-l = []
-for i in range(M):
-    a,b = input().split()
-    a = int(a)
-    l.append([a,b])
-for i in range(1,N+1):
-    st = ""
-    for j in range(M):
-        if i % l[j][0] == 0:
-            st = st + l[j][1] + " "
-    if st == "":
-        print(i)
-    else:
-        st = st.rstrip(" ")
-        print(st)
+N,D = map(int, input().split())
+sum = D
+for i in range(N-1):
+    z = int(input())
+    z = D - z
+    sum += z
+
+print(sum*D)
