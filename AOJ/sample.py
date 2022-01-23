3264bde63f3a819b495aae1ac523008b349b3364
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:33:54 +0900

    【キャンペーン問題】枠で囲む

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,8 +1,5 @@
-N,D = map(int, input().split())
-sum = D
-for i in range(N-1):
-    z = int(input())
-    z = D - z
-    sum += z
-
-print(sum*D)
+i = input()
+x = "+" * (len(i) +2)
+print(x)
+print("+%s+" %i)
+print(x)
