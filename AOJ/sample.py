f264953614f4c3655928a92ad9e3b29eb81f8877
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-23 20:37:43 +0900

    カラオケ大会

--- AOJ/sample.py
+++ AOJ/sample.py
@@ -1,18 +1,29 @@
-n,m,t,k = map(int,input().split()) # 発言数，監視時間，対象期間，回数
-cnt = 1+m-t
-l = [[0 for j in range(m)] for i in range(n)]
-for i in range(m):
-    kom = list(map(int,input().split()))
-    for j in range(n):
-        l[j][i] = kom[j]
+l1 = []
+l2 = []
+max_ans = 0
+n, le = map(int,input().split())
+for i in range((n+1)*le):
+    x = int(input())
+    if i < le:
+        l1.append(x)
+    else:
+        l2.append(x)
 for i in range(n):
-    for j in range(cnt):
-        l_a = l[i]
-        ans = sum(l_a[0:t])
-        l[i].remove(l[i][0])
-        if k - ans<= 0:
-            print("yes %d" %(j+t))
-            break
+    max = 100
+    for j in range(le):
+        z = l1[j] - l2[j]
+        if z == 0:
+            max = max
+        elif 0 < z < 11 or -11 < z < 0:
+            max -= 1
+        elif 10 < z < 21 or -21 < z < -10:
+            max -= 2
+        elif 20 < z < 31 or -31 < z < -20:
+            max -= 3
         else:
-            if j == cnt-1:
-                print("no 0")
+            max -= 5
+    if max_ans < max:
+        max_ans = max
+    for c in range(n):
+        l2.remove(l2[c])
+print((max_ans))
