e8bab81bd4a66344f3724e5709197690e4f42739
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-01-27 19:09:23 +0900

    チョコの分割

--- paiza/sample.py
+++ paiza/sample.py
@@ -1,8 +1,21 @@
-S = input()
-S = S.replace("-", "")
-x = S.count("0")
-sum = 0 + x * 12
-S = S.replace("0", "")
-for i in S:
-    sum += int(i) + 2
-print(sum*2)
+h,w = map(int, input().split())
+l = [[0]*w for i in range(h)]
+ans = []
+for i in range(h):
+    s = list(map(int, input().split()))
+    for j in range(w):
+        l[i][j] = s[j]
+for i in range(h):
+    for j in range(1,w+1):
+        a = sum(l[i][:j])
+        b = sum(l[i][j:])
+        if a == b:
+            ans.append(j)
+            break
+if ans == []:
+    print("No")
+else:
+    print("Yes")
+    for i in range(h):
+        a = ans[i]
+        print("A"*a+"B"*(w-a))
